51495b8618d5e6d6c1f84e6d1f6e050b793070eb
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-03-24 20:29:50 +0000

    import console created users into terraform
    Dan created some users in the console, they should exist in code
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
    Co-authored-by: Karl <karl.baker@digital.cabinet-office.gov.uk>
    Co-authored-by: Vitalie <vitalie.mogoreanu@digital.cabinet-office.gov.uk>
    Co-authored-by: Andy <andrew.sutton@ons.gov.uk>

--- terraform/deployments/corona-data-staging/ons-users/main.tf
+++ terraform/deployments/corona-data-staging/ons-users/main.tf
@@ -22,3 +22,57 @@ resource "aws_iam_user_policy_attachment" "dan_melluish_admin" {
   user       = "${aws_iam_user.dan_melluish.name}"
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+resource "aws_iam_user" "adam_douglas" {
+  name = "adam.douglas"
+}
+
+resource "aws_iam_user_policy_attachment" "adam_douglas_admin" {
+  user       = "${aws_iam_user.adam_douglas.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_user" "andy_sutton" {
+  name = "andy.sutton"
+}
+
+resource "aws_iam_user_policy_attachment" "andy_sutton_admin" {
+  user       = "${aws_iam_user.andy_sutton.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_user" "paul_groom" {
+  name = "paul.groom"
+}
+
+resource "aws_iam_user_policy_attachment" "paul_groom_admin" {
+  user       = "${aws_iam_user.paul_groom.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_user" "rhydian_page" {
+  name = "rhydian.page"
+}
+
+resource "aws_iam_user_policy_attachment" "rhydian_page_admin" {
+  user       = "${aws_iam_user.rhydian_page.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_user" "thanasis_anthopoulos" {
+  name = "thanasis.anthopoulos"
+}
+
+resource "aws_iam_user_policy_attachment" "thanasis_anthopoulos_admin" {
+  user       = "${aws_iam_user.thanasis_anthopoulos.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_user" "tom_mills" {
+  name = "tom.mills"
+}
+
+resource "aws_iam_user_policy_attachment" "tom_mills_admin" {
+  user       = "${aws_iam_user.tom_mills.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
