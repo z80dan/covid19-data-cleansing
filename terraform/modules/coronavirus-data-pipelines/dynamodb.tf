8f746cbfacff40c28b7de06f22c89fddbc0e3727
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-03-25 15:47:07 +0000

    remove dirty merge as it is not used
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
    Co-authored-by: Sam <sam.crang@digital.cabinet-office.gov.uk>
    Co-authored-by: Stephen <stephen.grier@digital.cabinet-office.gov.uk>
    Co-authored-by: Stephen <stephen.harker@digital.cabinet-office.gov.uk>

--- terraform/modules/coronavirus-data-pipelines/dynamodb.tf
+++ terraform/modules/coronavirus-data-pipelines/dynamodb.tf
@@ -1,29 +1,9 @@
 // imported resources
 // import the dynamo db tables from the IVR and web form
-
 data "aws_dynamodb_table" "ivr_submissions" {
   name = "UKCustomerResponseTable"
 }
 
 data "aws_dynamodb_table" "web_submissions" {
   name = "coronavirus-vulnerable-people-${var.deployment}"
-}
-
-
-// created resources
-resource "aws_dynamodb_table" "dirty_merge" {
-  name           = "corona-data-pipelines-dirty-merge-${var.deployment}"
-
-  billing_mode   = "PAY_PER_REQUEST"
-
-  attribute {
-    name = "NHSNumber"
-    type = "S"
-  }
-
-  hash_key       = "NHSNumber"
-}
-
-output "dirty_merge_db_table_name" {
-  value = "${aws_dynamodb_table.dirty_merge.name}"
 }
