11164afa2539319237d3065f2f11b6d4bb976854
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-03-25 01:49:02 +0000

    Fix bug in web form import script
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- terraform/modules/coronavirus-data-pipelines/files/web-form-import.py
+++ terraform/modules/coronavirus-data-pipelines/files/web-form-import.py
@@ -24,7 +24,7 @@ datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "${databa
 ## @inputs: [frame = datasource0]
 applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("formresponse", "struct", "formresponse", "struct"), ("updated_at", "double", "updated_at", "double"), ("referenceid", "string", "referenceid", "string"), ("unixtimestamp", "double", "unixtimestamp", "double"), ("created_at", "double", "created_at", "double")], transformation_ctx = "applymapping1")
 ## @type: DataSink
-## @args: [connection_type = "s3", connection_options = {"path": "s3://${sink_bucket}/web-form-import"}, format = "json", transformation_ctx = "datasink2"]
+## @args: [connection_type = "s3", connection_options = {"path": "s3://${sink_bucket}/sink"}, format = "json", transformation_ctx = "datasink2"]
 ## @return: datasink2
 ## @inputs: [frame = applymapping1]
 datasink2 = glueContext.write_dynamic_frame.from_options(frame = applymapping1, connection_type = "s3", connection_options = {"path": "s3://${sink_bucket}/sink"}, format = "json", transformation_ctx = "datasink2")
